91cbc70f5d99b09e124aea1b8e5a627ed27c3402
Bartosz Jurga <barti.jurga@gmail.com>
2016-07-25 22:28:09 +0200

    Add project files.

---
+++ ProjektPuszcze/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjektPuszcze.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace ProjektPuszcze.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Dzialalnosc()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("barti.jurga@gmail.com"));
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+                using (var smtp = new SmtpClient())
+                {
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return PartialView("_MailFormView", model);
+        }
+        public ActionResult Sent()
+        {
+            return PartialView("_SentView");
+        }
+    }
+}
