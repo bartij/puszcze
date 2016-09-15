78f1be95ea672f44442104a41f07936f376bc62d
Bartosz Jurga <barti.jurga@gmail.com>
2016-09-15 18:11:14 +0200

    site ready without contact

--- ProjektPuszcze/Controllers/HomeController.cs
+++ ProjektPuszcze/Controllers/HomeController.cs
@@ -21,30 +21,5 @@ namespace ProjektPuszcze.Controllers
         {
             return View();
         }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Contact(EmailModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
-                var message = new MailMessage();
-                message.To.Add(new MailAddress("barti.jurga@gmail.com"));
-                message.Subject = "Your email subject";
-                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
-                message.IsBodyHtml = true;
-                using (var smtp = new SmtpClient())
-                {
-                    await smtp.SendMailAsync(message);
-                    return RedirectToAction("Sent");
-                }
-            }
-            return PartialView("_MailFormView", model);
-        }
-        public ActionResult Sent()
-        {
-            return PartialView("_SentView");
-        }
     }
 }
